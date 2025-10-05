93898d8c58ca883fedeb9af1c667161e1929991b
CitizenOfMars <sidneysychan007@gmail.com>
2025-10-05 16:15:23 +0800

    edit data extraction

--- etl.py
+++ etl.py
@@ -14,18 +14,28 @@ data = []
 for record in root.findall('./data/record'):
     record_data = {}
     for field in record.findall('field'):
+        if field.attrib['name'] == 'Item':
+            continue
         record_data[field.attrib['name']] = field.text
     data.append(record_data)
+    
 
 pop_data = pd.DataFrame(data)
+pop_data.rename(columns={'Value': 'Population'}, inplace=True)
 
 
 print("--- Quality of Life Data Columns ---")
 print(quality_data.columns)
+print("\nData Preview:")
+print(quality_data.head())
 print("\n--- GDP Data Columns ---")
 print(gdp_data.columns)
+print("\nData Preview:")
+print(gdp_data.head())
 print("\n--- Population Data from XML ---")
 print(f"Shape: {pop_data.shape}")
 print("Columns:", pop_data.columns)
 print("Data Preview:")
 print(pop_data.head())
+
+# Data extraction complete
